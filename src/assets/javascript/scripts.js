b0f077e1b32b7060d2ece1f803f0a4c52865b38f
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-01-15 23:41:18 +0200

    ddd

--- src/assets/javascript/scripts.js
+++ src/assets/javascript/scripts.js
@@ -1,14 +1,1 @@
-$(document).ready(function() {
-
-  //Add/remove background for navbar(once you scroll down)
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 5) {
-      $(".navbar").addClass("navbar--animated");
-    } else {
-      $(".navbar").removeClass("navbar--animated");
-    }
-  });
-
-});
+$(document).ready(function() {});
